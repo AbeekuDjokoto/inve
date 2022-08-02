025218ec870bc7c68696052383b7107357396c75
AbeekuDjokoto <75487734+AbeekuDjokoto@users.noreply.github.com>
2022-08-02 16:49:59 -0700

    Delete routes directory

--- routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { validateProduct } from '../middleware/products.js'
-import { addProduct } from "../controllers/products.js"
-const router = Router();
-
-router.post("/products", validateProduct, addProduct);
-
-export default router;
